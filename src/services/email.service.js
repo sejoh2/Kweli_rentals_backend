9aa03dbeb8e1fc918cc8f71b41beb9c91e23d04c
joseh33 <josehsejoh33@gmail.com>
2026-04-14 12:27:49 +0300

    ...

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -9,12 +9,11 @@ const passwordResetCodes = new Map();
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4,
 });
 
 // Generate 6-digit verification code
